7e718f99b47b57b99b6e8aedcd2f1c0e4a05f71c
RAHAT11790 <rahatsarker224@gmail.com>
2025-07-14 06:39:17 +0600

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ API_HASH = environ.get("API_HASH", "8ba23141980539b4896e5adbc4ffd2e2")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7871528004:AAGvbpoZ2bM2Oor7vH8Zo9vBB-0d4oZwjL8")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6621572366').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "RS ANIME 04") # without @
 PORT = environ.get("PORT", "8080")
 
